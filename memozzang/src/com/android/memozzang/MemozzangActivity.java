0c9c19ccd46c9ea4e77ecdc56cf6fb9bb52c6d9d
Sunyoung yu <ysy1990117@gmail.com>
2012-05-26 16:19:59 +0900

    d

--- memozzang/src/com/android/memozzang/MemozzangActivity.java
+++ memozzang/src/com/android/memozzang/MemozzangActivity.java
@@ -8,6 +8,6 @@ public class MemozzangActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layouddddddddt.main);
     }
 }
